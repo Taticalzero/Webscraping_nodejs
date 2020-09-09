d7c11e7f78b628166d50793dd37bd4a1d1ebc26b
Taticalzero <52974709+Taticalzero@users.noreply.github.com>
2020-09-09 11:19:31 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+const puppeter = require('puppeteer');
+
+(async () => {
+
+    let filmeUrl = 'https://www.imdb.com/title/tt6723592/?ref_=hm_fanfav_tt_1_pd_fp1';
+    
+    let browser = await puppeter.launch({headless: false});
+    let page = await browser.newPage();
+
+    await page.goto(filmeUrl, { waitUntil:'networkidle2'});
+
+    let dados = await page.evaluate(() =>{
+        let titulo = document.querySelector('div[class="title_wrapper"] > h1').innerText;
+        let nota = document.querySelector('span[itemprop="ratingValue"]').innerText;
+        let avaliacao = document.querySelector('span[itemprop="ratingCount"]').innerText;
+
+        return{
+            titulo,
+            nota,
+            avaliacao,
+        }
+    })
+
+    console.log(dados);
+    debugger;
+
+    await browser.close();
+})();
